c7eb082a08374e10eb998b037d3377ea3dee8a4a
shirshovavika <shirshova_vikzz@list.ru>
2019-12-23 19:55:19 +0700

    ещё

--- Program7.cs
+++ Program7.cs
@@ -23,7 +23,7 @@ namespace Практическая_7
             for (int i = 0; i < array.GetLength(0); i++)
             {
                 int secondArray = array[i, i];
-                for (int j = 0; j < array.GetLength(0); j++)
+                for (int j = 0; j < array.GetLength(1); j++)
                 {
                     Console.Write((array[i, j] += secondArray) + " ");
                 }
